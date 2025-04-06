1b545903276ad84b0efa3ff352831820b9e4b1b3
Moksh Malde <maldemoksh437@gmail.com>
2025-04-06 09:09:06 +0530

    refactor: update axios configuration and add API export

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,1 +1,9 @@
-import axios from 'axios'
+// utils/axios.js
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:8000/api",
+  withCredentials: true, // httpOnly cookies enabled
+});
+
+export default API;
